917593b594be6a597f8f50397cd6843bd8fdb77d
Robert Jones II <rjones@corporatewebimage.com>
2020-07-31 17:26:54 -0400

    Fixed function that had been incorrectly renamed

--- libraries/xml/xslt.php
+++ libraries/xml/xslt.php
@@ -13,7 +13,7 @@ class CWI_XML_XSLT_Template {
 	}
 }
 
-function __constructTemplateStackAdd(&$stack, $element) {
+function CWI_XML_XsltTemplateStackAdd(&$stack, $element) {
 	for ($i=count($stack)-1; $i >= 0; $i --) {
 		$calc = $element['priority'] - $stack[$i]['priority'];
 		if ($calc >= 0) {
